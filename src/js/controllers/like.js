201f427dd1057e3390ebf82abf9220c39714dbbd
Tyler Laurenceau <TylerSLaurenceau@gmail.com>
2017-03-13 16:12:41 -0400

    like now changes from plural to singular

--- src/js/controllers/like.js
+++ src/js/controllers/like.js
@@ -1,10 +1,19 @@
+import $ from 'jquery'
+
 function LikeController($scope){
   console.log("well hello there!");
 
   $scope.like = 0;
+  $scope.name = 'likes'
 
   $scope.addLike = function () {
     $scope.like += 1;
+    if ($scope.like === 1){
+      $scope.name = 'like'
+    }
+    else if ($scope.like != 1){
+      $scope.name = 'likes'
+    }
   }
 
   $scope.removeLike = function () {

d6efc9229e2182438621bef33c02953b4866f3cf
Tyler Laurenceau <TylerSLaurenceau@gmail.com>
2017-03-13 17:48:48 -0400

    added some styling

--- src/js/controllers/like.js
+++ src/js/controllers/like.js
@@ -5,19 +5,28 @@ function LikeController($scope){
 
   $scope.like = 0;
   $scope.name = 'likes'
+  $scope.toggle = false;
 
   $scope.addLike = function () {
-    $scope.like += 1;
-    if ($scope.like === 1){
-      $scope.name = 'like'
-    }
-    else if ($scope.like != 1){
+    if ($scope.toggle === false){
+      $scope.like += 1;
       $scope.name = 'likes'
+      if ($scope.like === 1){
+        $scope.name = 'like'
+      }
+    }
+    else if ($scope.toggle === true){
+      $scope.like -= 1;
     }
   }
 
-  $scope.removeLike = function () {
-    $scope.like -= 1;
+  $scope.switch = function () {
+    if ($scope.toggle === false){
+      $scope.toggle = true
+    }
+    else if ($scope.toggle === true){
+      $scope.toggle = false
+    }
   }
 }
 
